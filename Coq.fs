b7e2cf5c15a4747be789d6f01617a5abac0739b9
Colin Stebbins Gordon <csgordon@cs.washington.edu>
2013-08-11 18:13:30 -0700

    CoqInCoq's core.ml translates with our script & extraction fix

--- Coq.fs
+++ Coq.fs
@@ -1,3 +1,11 @@
-(* Mostly F# implementations of OCaml standard library components *)
+module Coq 
 
-let string_of_int (i:int) : string = System.Convert.ToString(i)
+  (* Mostly F# implementations of OCaml standard library components *)
+  
+  let string_of_int (i:int) : string = System.Convert.ToString(i)
+  
+  let succ (i:int) : int = i + 1
+  
+  let rec ml_assert (b:bool) : 'a =
+      assert b;
+      ml_assert b;;
---
+++ ml2fs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIRNAME=`dirname $1`
+BASENAME=`basename --suffix .ml $1`
+FILE="${DIRNAME}/${BASENAME}.fs"
+
+touch $FILE
+echo module ${BASENAME} > $FILE
+echo >> $FILE
+echo open Coq >> $FILE
+echo >> $FILE
+cat $1 | sed -e 's/ assert / ml_assert /g' \
+       | sed -e 's/^/  /' >> $FILE
+fsharpc --mlcompatibility -a -r:Coq.dll $FILE
